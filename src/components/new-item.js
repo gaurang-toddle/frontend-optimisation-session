8c273ed3a9a881c2441c550b8f228dd8d73c737c
parakh-toddleapp <parakh.srivastava@toddleapp.com>
2024-01-10 22:17:49 +0530

    removed extra spaces

--- src/components/new-item.js
+++ src/components/new-item.js
@@ -35,8 +35,6 @@ const NewItem = ({ addItem }) => {
             ➕ Add New Item
           </button>
         </div>
-        
-
       </form>
       <ExpensiveComponent />
       <div className="px-4 py-2 font-bold text-center bg-blue-400 border border-red-700 animate-pulse">
