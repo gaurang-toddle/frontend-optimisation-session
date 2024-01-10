1243c394c258ce3391a504122628636873173857
Vaibhav Sorathiya <vaibhav@toddleapp.com>
2024-01-10 15:09:41 +0530

    Add: normalized state of places

--- src/statesApp/Places.js
+++ src/statesApp/Places.js
@@ -516,3 +516,592 @@ export const initialTravelPlan = {
         },
     ],
 };
+
+export const initialTravelPlanNormalized = {
+    "0": {
+        "id": 0,
+        "title": "(Root)",
+        "childIds": [
+            1,
+            42,
+            46
+        ]
+    },
+    "1": {
+        "id": 1,
+        "title": "Earth",
+        "latitude": 0,
+        "longitude": 0,
+        "childIds": [
+            2,
+            10,
+            20,
+            24,
+            25,
+            26,
+            27,
+            28
+        ]
+    },
+    "2": {
+        "id": 2,
+        "title": "Africa",
+        "latitude": 8.7832,
+        "longitude": 34.5085,
+        "childIds": [
+            7,
+            8
+        ]
+    },
+    "7": {
+        "id": 7,
+        "title": "Morocco",
+        "latitude": 31.7917,
+        "longitude": -7.0926,
+        "childIds": [
+            701,
+            702,
+            703,
+            704
+        ]
+    },
+    "8": {
+        "id": 8,
+        "title": "Kenya",
+        "latitude": -1.286389,
+        "longitude": 36.817223,
+        "childIds": [
+            801,
+            802,
+            803,
+            804
+        ]
+    },
+    "10": {
+        "id": 10,
+        "title": "Americas",
+        "latitude": 37.0902,
+        "longitude": -95.7129,
+        "childIds": [
+            15,
+            16
+        ]
+    },
+    "15": {
+        "id": 15,
+        "title": "Mexico",
+        "latitude": 23.6345,
+        "longitude": -102.5528,
+        "childIds": [
+            1501,
+            1502,
+            1503,
+            1504
+        ]
+    },
+    "16": {
+        "id": 16,
+        "title": "Colombia",
+        "latitude": 4.5709,
+        "longitude": -74.2973,
+        "childIds": [
+            1601,
+            1602,
+            1603,
+            1604
+        ]
+    },
+    "20": {
+        "id": 20,
+        "title": "Asia",
+        "latitude": 34.0479,
+        "longitude": 100.6197,
+        "childIds": [
+            21,
+            22,
+            23
+        ]
+    },
+    "21": {
+        "id": 21,
+        "title": "China",
+        "latitude": 35.8617,
+        "longitude": 104.1954,
+        "childIds": [
+            2101,
+            2102,
+            2103,
+            2104
+        ]
+    },
+    "22": {
+        "id": 22,
+        "title": "India",
+        "latitude": 20.5937,
+        "longitude": 78.9629,
+        "childIds": [
+            2201,
+            2202,
+            2203,
+            2204
+        ]
+    },
+    "23": {
+        "id": 23,
+        "title": "Japan",
+        "latitude": 36.2048,
+        "longitude": 138.2529,
+        "childIds": [
+            2301,
+            2302,
+            2303,
+            2304
+        ]
+    },
+    "24": {
+        "id": 24,
+        "title": "France",
+        "latitude": 46.6035,
+        "longitude": 1.8883,
+        "childIds": [
+            2401,
+            2402,
+            2403,
+            2404
+        ]
+    },
+    "25": {
+        "id": 25,
+        "title": "Germany",
+        "latitude": 51.1657,
+        "longitude": 10.4515,
+        "childIds": [
+            2501,
+            2502,
+            2503,
+            2504
+        ]
+    },
+    "26": {
+        "id": 26,
+        "title": "Australia",
+        "latitude": -25.2744,
+        "longitude": 133.7751,
+        "childIds": [
+            2601,
+            2602,
+            2603,
+            2604
+        ]
+    },
+    "27": {
+        "id": 27,
+        "title": "Ontario",
+        "latitude": 51.2538,
+        "longitude": -85.3232,
+        "childIds": [
+            2701,
+            2702,
+            2703
+        ]
+    },
+    "28": {
+        "id": 28,
+        "title": "Quebec",
+        "latitude": 46.8139,
+        "longitude": -71.208,
+        "childIds": [
+            2801,
+            2802,
+            2803
+        ]
+    },
+    "42": {
+        "id": 42,
+        "title": "Moon",
+        "latitude": 20.428,
+        "longitude": 86.318,
+        "childIds": [
+            43
+        ]
+    },
+    "43": {
+        "id": 43,
+        "title": "Rheita",
+        "latitude": -5.3498,
+        "longitude": -16.7291,
+        "childIds": [
+            4301,
+            4302
+        ]
+    },
+    "46": {
+        "id": 46,
+        "title": "Mars",
+        "latitude": 1.3521,
+        "longitude": 103.8198,
+        "childIds": [
+            47
+        ]
+    },
+    "47": {
+        "id": 47,
+        "title": "Corn Town",
+        "latitude": -4.0383,
+        "longitude": 137.361,
+        "childIds": [
+            4701,
+            4702
+        ]
+    },
+    "701": {
+        "id": 701,
+        "title": "Casablanca",
+        "latitude": 33.5731,
+        "longitude": -7.5898,
+        "childIds": []
+    },
+    "702": {
+        "id": 702,
+        "title": "Fez",
+        "latitude": 34.0181,
+        "longitude": -5.0074,
+        "childIds": []
+    },
+    "703": {
+        "id": 703,
+        "title": "Marrakesh",
+        "latitude": 31.6295,
+        "longitude": -7.9811,
+        "childIds": []
+    },
+    "704": {
+        "id": 704,
+        "title": "Rabat",
+        "latitude": 34.0209,
+        "longitude": -6.8416,
+        "childIds": []
+    },
+    "801": {
+        "id": 801,
+        "title": "Nairobi",
+        "latitude": -1.286389,
+        "longitude": 36.817223,
+        "childIds": []
+    },
+    "802": {
+        "id": 802,
+        "title": "Mombasa",
+        "latitude": -4.0435,
+        "longitude": 39.6682,
+        "childIds": []
+    },
+    "803": {
+        "id": 803,
+        "title": "Kisumu",
+        "latitude": -0.1022,
+        "longitude": 34.7617,
+        "childIds": []
+    },
+    "804": {
+        "id": 804,
+        "title": "Nakuru",
+        "latitude": -0.3031,
+        "longitude": 36.08,
+        "childIds": []
+    },
+    "1501": {
+        "id": 1501,
+        "title": "Mexico City",
+        "latitude": 19.4326,
+        "longitude": -99.1332,
+        "childIds": []
+    },
+    "1502": {
+        "id": 1502,
+        "title": "Guadalajara",
+        "latitude": 20.6597,
+        "longitude": -103.3496,
+        "childIds": []
+    },
+    "1503": {
+        "id": 1503,
+        "title": "Monterrey",
+        "latitude": 25.439,
+        "longitude": -100.9737,
+        "childIds": []
+    },
+    "1504": {
+        "id": 1504,
+        "title": "Puebla",
+        "latitude": 19.0402,
+        "longitude": -98.198,
+        "childIds": []
+    },
+    "1601": {
+        "id": 1601,
+        "title": "Bogota",
+        "latitude": 4.711,
+        "longitude": -74.0721,
+        "childIds": []
+    },
+    "1602": {
+        "id": 1602,
+        "title": "Medellin",
+        "latitude": 6.2442,
+        "longitude": -75.5814,
+        "childIds": []
+    },
+    "1603": {
+        "id": 1603,
+        "title": "Cali",
+        "latitude": 3.4516,
+        "longitude": -76.532,
+        "childIds": []
+    },
+    "1604": {
+        "id": 1604,
+        "title": "Barranquilla",
+        "latitude": 10.9639,
+        "longitude": -74.7964,
+        "childIds": []
+    },
+    "2101": {
+        "id": 2101,
+        "title": "Beijing",
+        "latitude": 39.9042,
+        "longitude": 116.4074,
+        "childIds": []
+    },
+    "2102": {
+        "id": 2102,
+        "title": "Shanghai",
+        "latitude": 31.2304,
+        "longitude": 121.4737,
+        "childIds": []
+    },
+    "2103": {
+        "id": 2103,
+        "title": "Guangzhou",
+        "latitude": 23.1291,
+        "longitude": 113.2644,
+        "childIds": []
+    },
+    "2104": {
+        "id": 2104,
+        "title": "Shenzhen",
+        "latitude": 22.5431,
+        "longitude": 114.0579,
+        "childIds": []
+    },
+    "2201": {
+        "id": 2201,
+        "title": "Mumbai",
+        "latitude": 19.076,
+        "longitude": 72.8777,
+        "childIds": []
+    },
+    "2202": {
+        "id": 2202,
+        "title": "Delhi",
+        "latitude": 28.6139,
+        "longitude": 77.209,
+        "childIds": []
+    },
+    "2203": {
+        "id": 2203,
+        "title": "Bengaluru",
+        "latitude": 12.9716,
+        "longitude": 77.5946,
+        "childIds": []
+    },
+    "2204": {
+        "id": 2204,
+        "title": "Chennai",
+        "latitude": 13.0827,
+        "longitude": 80.2707,
+        "childIds": []
+    },
+    "2301": {
+        "id": 2301,
+        "title": "Tokyo",
+        "latitude": 35.6895,
+        "longitude": 139.6917,
+        "childIds": []
+    },
+    "2302": {
+        "id": 2302,
+        "title": "Yokohama",
+        "latitude": 35.4437,
+        "longitude": 139.638,
+        "childIds": []
+    },
+    "2303": {
+        "id": 2303,
+        "title": "Osaka",
+        "latitude": 34.6937,
+        "longitude": 135.5023,
+        "childIds": []
+    },
+    "2304": {
+        "id": 2304,
+        "title": "Nagoya",
+        "latitude": 35.1815,
+        "longitude": 136.9066,
+        "childIds": []
+    },
+    "2401": {
+        "id": 2401,
+        "title": "Paris",
+        "latitude": 48.8566,
+        "longitude": 2.3522,
+        "childIds": []
+    },
+    "2402": {
+        "id": 2402,
+        "title": "Marseille",
+        "latitude": 43.2965,
+        "longitude": 5.3698,
+        "childIds": []
+    },
+    "2403": {
+        "id": 2403,
+        "title": "Lyon",
+        "latitude": 45.7578,
+        "longitude": 4.832,
+        "childIds": []
+    },
+    "2404": {
+        "id": 2404,
+        "title": "Toulouse",
+        "latitude": 43.6047,
+        "longitude": 1.4442,
+        "childIds": []
+    },
+    "2501": {
+        "id": 2501,
+        "title": "Berlin",
+        "latitude": 52.52,
+        "longitude": 13.405,
+        "childIds": []
+    },
+    "2502": {
+        "id": 2502,
+        "title": "Hamburg",
+        "latitude": 53.5511,
+        "longitude": 9.9937,
+        "childIds": []
+    },
+    "2503": {
+        "id": 2503,
+        "title": "Munich",
+        "latitude": 48.8566,
+        "longitude": 2.3522,
+        "childIds": []
+    },
+    "2504": {
+        "id": 2504,
+        "title": "Cologne",
+        "latitude": 50.9375,
+        "longitude": 6.9603,
+        "childIds": []
+    },
+    "2601": {
+        "id": 2601,
+        "title": "Sydney",
+        "latitude": -33.8688,
+        "longitude": 151.2093,
+        "childIds": []
+    },
+    "2602": {
+        "id": 2602,
+        "title": "Melbourne",
+        "latitude": -37.8136,
+        "longitude": 144.9631,
+        "childIds": []
+    },
+    "2603": {
+        "id": 2603,
+        "title": "Brisbane",
+        "latitude": -27.4698,
+        "longitude": 153.0251,
+        "childIds": []
+    },
+    "2604": {
+        "id": 2604,
+        "title": "Perth",
+        "latitude": -31.9505,
+        "longitude": 115.8605,
+        "childIds": []
+    },
+    "2701": {
+        "id": 2701,
+        "title": "Toronto",
+        "latitude": 43.6532,
+        "longitude": -79.3832,
+        "childIds": []
+    },
+    "2702": {
+        "id": 2702,
+        "title": "Ottawa",
+        "latitude": 45.4215,
+        "longitude": -75.6993,
+        "childIds": []
+    },
+    "2703": {
+        "id": 2703,
+        "title": "Mississauga",
+        "latitude": 43.589,
+        "longitude": -79.6441,
+        "childIds": []
+    },
+    "2801": {
+        "id": 2801,
+        "title": "Montreal",
+        "latitude": 45.5017,
+        "longitude": -73.5673,
+        "childIds": []
+    },
+    "2802": {
+        "id": 2802,
+        "title": "Quebec City",
+        "latitude": 46.8139,
+        "longitude": -71.208,
+        "childIds": []
+    },
+    "2803": {
+        "id": 2803,
+        "title": "Laval",
+        "latitude": 45.5695,
+        "longitude": -73.692,
+        "childIds": []
+    },
+    "4301": {
+        "id": 4301,
+        "title": "Rheita Valley",
+        "latitude": -4.5233,
+        "longitude": -16.632,
+        "childIds": []
+    },
+    "4302": {
+        "id": 4302,
+        "title": "Rheita Plateau",
+        "latitude": -4.6765,
+        "longitude": -16.8035,
+        "childIds": []
+    },
+    "4701": {
+        "id": 4701,
+        "title": "Corn Fields",
+        "latitude": -4.0785,
+        "longitude": 137.3229,
+        "childIds": []
+    },
+    "4702": {
+        "id": 4702,
+        "title": "Corn Hills",
+        "latitude": -4.0162,
+        "longitude": 137.3667,
+        "childIds": []
+    }
+};
